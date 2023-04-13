f8094ca08b45f14fb04238ef5f33002255469c47
RaulLoyola <raul.loy@gmail.com>
2023-04-13 12:51:18 -0600

    Fixing HubSpot contacts bug

--- src/data/hubspot.js
+++ src/data/hubspot.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const contactsData = async (since, until) => {
   const response = await axios.get(
-    `/api/contacts-by-time-range?since=${since}&until=${until}`
+    `https://www.dashboard-aws.net/api/contacts-by-time-range?since=${since}&until=${until}`
   );
   return response.data;
 };
