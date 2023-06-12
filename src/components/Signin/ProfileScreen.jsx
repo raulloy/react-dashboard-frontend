e052786a4b7317305719022bb556fdc007382b24
RaulLoyola <raul.loy@gmail.com>
2023-06-12 12:20:57 -0600

    Adding profile screen

---
+++ src/components/Signin/ProfileScreen.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { toast } from 'react-toastify';
+
+import axios from 'axios';
+import { getError } from '../../utils';
+
+export default function ProfileScreen() {
+  const userInfo = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null;
+
+  const [name, setName] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        'https://www.dashboard-aws.net/api/users/profile',
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      document.location.reload();
+      toast.success('User updated successfully');
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  const signoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('userInfo');
+    document.location.reload();
+  };
+
+  return (
+    <div className="container small-container">
+      <h1 className="my-3">User Profile</h1>
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Update</Button>
+        </div>
+        <div className="mb-3">
+          <Button type="submit" onClick={signoutHandler}>
+            Log out
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
