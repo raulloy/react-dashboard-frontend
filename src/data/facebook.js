1fed837c1e35b11bc6408ce5b219bff8e168e43d
RaulLoyola <raul.loy@gmail.com>
2023-07-18 09:10:13 -0600

    Fixing FB and HubSpot API Data

--- src/data/facebook.js
+++ src/data/facebook.js
@@ -2,28 +2,28 @@ import axios from 'axios';
 
 export const accountsData = async (since, until) => {
   const response = await axios.get(
-    `/api/account-insights?since=${since}&until=${until}`
+    `https://www.dashboard-aws.net/api/account-insights?since=${since}&until=${until}`
   );
   return response.data;
 };
 
 export const campaignsData = async (selectedAccount, since, until) => {
   const response = await axios.get(
-    `/api/campaign-insights/${selectedAccount}?since=${since}&until=${until}`
+    `https://www.dashboard-aws.net/api/campaign-insights/${selectedAccount}?since=${since}&until=${until}`
   );
   return response.data;
 };
 
 export const adSetsData = async (selectedAccount, since, until) => {
   const response = await axios.get(
-    `/api/adsets-insights/${selectedAccount}?since=${since}&until=${until}`
+    `https://www.dashboard-aws.net/api/adsets-insights/${selectedAccount}?since=${since}&until=${until}`
   );
   return response.data;
 };
 
 export const adsData = async (selectedAccount, since, until) => {
   const response = await axios.get(
-    `/api/ads-insights/${selectedAccount}?since=${since}&until=${until}`
+    `https://www.dashboard-aws.net/api/ads-insights/${selectedAccount}?since=${since}&until=${until}`
   );
   return response.data;
 };
