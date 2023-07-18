e1f2f8a4ea2a8eba9fa0788c1f7018560589aca7
RaulLoyola <raul.loy@gmail.com>
2023-07-18 08:37:07 -0600

    Adding Google Assignments to general report

--- src/components/Table/General.jsx
+++ src/components/Table/General.jsx
@@ -4,25 +4,161 @@ import DateRangeInput from '../DatePickers/DateRangeInput';
 import { AccountsDataStoreContext } from '../../data/AccountsDataStore';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { accounts, googleAccounts } from '../../data/data';
-import { GoogleSpendByAccount } from '../../data/GoogleCampaignsData';
+import {
+  GoogleCPCByAccount,
+  GoogleDataByAccount,
+  GoogleLeadsByAccount,
+  GoogleSpendByAccount,
+} from '../../data/GoogleCampaignsData';
 import './Table.css';
 
 export default function GeneralTable() {
-  const { since, setSince, until, setUntil, accountInsights } = useContext(
-    AccountsDataStoreContext
-  );
+  const { since, setSince, until, setUntil, accountInsights, contacts } =
+    useContext(AccountsDataStoreContext);
 
   // Calculate Grand Total Spend
   const grandTotalSpend = accountInsights.reduce((total, element) => {
     return total + parseFloat(element?.spend);
   }, 0);
 
-  // Calculate Grand Total CPC
-  const grandTotalCPC =
-    accountInsights.reduce((acc, curr) => acc + parseFloat(curr?.cpc ?? 0), 0) /
-    accountInsights.length;
+  const fbContacts = contacts.filter(
+    (element) =>
+      element.properties.hs_analytics_first_url &&
+      element.properties.hs_analytics_first_url.includes('facebook.com')
+  );
 
-  console.table(grandTotalCPC);
+  const contactsbyCampaign = fbContacts.map(({ id, properties }) => ({
+    id,
+    hs_analytics_first_url: properties.hs_analytics_first_url
+      ? properties.hs_analytics_first_url.match(/hsa_acc=(\d+)/)?.[1]
+      : null,
+    lifecyclestage: properties.lifecyclestage,
+  }));
+
+  const huAccounts = accountInsights.filter((insight) => {
+    return accounts.some(
+      (account) =>
+        account.id.substring(4) === insight.account_id &&
+        account.company === 'HOGARES UNION'
+    );
+  });
+
+  const googleContacts = contacts.filter(
+    (element) =>
+      element.properties.hs_analytics_first_url &&
+      element.properties.hs_analytics_first_url.includes('ads.google.com')
+  );
+
+  const contactsbyGoogleCampaign = googleContacts.map(({ id, properties }) => ({
+    id,
+    hs_analytics_first_url: properties.hs_analytics_first_url
+      ? properties.hs_analytics_first_url.match(/hsa_cam=(\d+)/)?.[1]
+      : null,
+    lifecyclestage: properties.lifecyclestage,
+  }));
+
+  const huAssignments = huAccounts
+    .map((item) => {
+      const result = contactsbyCampaign.filter(
+        (account) => account.hs_analytics_first_url === item.account_id
+      );
+      return result;
+    })
+    .flat().length;
+
+  const huLifestyleAccounts = accountInsights.filter((insight) => {
+    return accounts.some(
+      (account) =>
+        account.id.substring(4) === insight.account_id &&
+        account.company === 'HU LIFESTYLE'
+    );
+  });
+
+  const huLifestyleAssignments = huLifestyleAccounts
+    .map((item) => {
+      const result = contactsbyCampaign.filter(
+        (account) => account.hs_analytics_first_url === item.account_id
+      );
+      return result;
+    })
+    .flat().length;
+
+  const gimAccounts = accountInsights.filter((insight) => {
+    return accounts.some(
+      (account) =>
+        account.id.substring(4) === insight.account_id &&
+        account.company === 'GIM LIVINGSPACES'
+    );
+  });
+
+  const gimAssignments = gimAccounts
+    .map((item) => {
+      const result = contactsbyCampaign.filter(
+        (account) => account.hs_analytics_first_url === item.account_id
+      );
+      return result;
+    })
+    .flat().length;
+
+  const huLeads = huAccounts.reduce(
+    (acc, curr) =>
+      acc +
+      parseFloat(
+        (curr?.actions.find((element) => element.action_type === 'lead') || {})
+          .value ?? 0
+      ),
+    0
+  );
+
+  const huLifestyleLeads = huLifestyleAccounts.reduce(
+    (acc, curr) =>
+      acc +
+      parseFloat(
+        (curr?.actions.find((element) => element.action_type === 'lead') || {})
+          .value ?? 0
+      ),
+    0
+  );
+
+  const gimLeads = gimAccounts.reduce(
+    (acc, curr) =>
+      acc +
+      parseFloat(
+        (curr?.actions.find((element) => element.action_type === 'lead') || {})
+          .value ?? 0
+      ),
+    0
+  );
+
+  // const googleAssignments = googleAccounts
+  //   .map((company) => {
+  //     const googleContacts = GoogleDataByAccount(since, until, company.id).map(
+  //       (company) => {
+  //         const result = contactsbyGoogleCampaign.filter(
+  //           (campaign) =>
+  //             campaign.hs_analytics_first_url === company.campaign.id.toString()
+  //         );
+  //         return result;
+  //       }
+  //     );
+  //     return googleContacts;
+  //   })
+  //   .flat()
+  //   .reduce((acc, arr) => acc + arr.length, 0);
+
+  // console.log(googleAssignments);
+
+  // console.log(
+  //   GoogleDataByAccount(since, until, '5347167145')
+  //     .map((company) => {
+  //       const result = contactsbyGoogleCampaign.filter(
+  //         (campaign) =>
+  //           campaign.hs_analytics_first_url === company.campaign.id.toString()
+  //       );
+  //       return result;
+  //     })
+  //     .flat().length
+  // );
 
   const totalSpendByCompany = accountInsights.reduce((result, element) => {
     const { account_name, spend } = element;
@@ -37,9 +173,9 @@ export default function GeneralTable() {
     return result;
   }, {});
 
-  if (accountInsights.length === 0) {
-    return <div>Getting data...</div>;
-  }
+  // if (accountInsights.length === 0) {
+  //   return <div>Getting data...</div>;
+  // }
 
   const fbColumns = [
     {
@@ -50,15 +186,44 @@ export default function GeneralTable() {
     },
     {
       field: 'company',
-      headerName: 'Campaña',
-      width: 300,
-      renderHeader: () => <div className="header-bold">Campaña</div>,
+      headerName: 'Compañia',
+      width: 280,
+      renderHeader: () => <div className="header-bold">Compañia</div>,
     },
     {
       field: 'totalSpend',
       headerName: 'Gastado',
+      type: 'number',
       width: 160,
       renderHeader: () => <div className="header-bold">Gastado</div>,
+    },
+    {
+      field: 'cpc',
+      headerName: 'Gastado',
+      type: 'number',
+      width: 160,
+      renderHeader: () => <div className="header-bold">CPC promedio</div>,
+    },
+    {
+      field: 'leads',
+      headerName: 'Leads',
+      type: 'number',
+      width: 160,
+      renderHeader: () => <div className="header-bold">Leads</div>,
+    },
+    {
+      field: 'cpl',
+      headerName: 'CPL',
+      type: 'number',
+      width: 160,
+      renderHeader: () => <div className="header-bold">CPL</div>,
+    },
+    {
+      field: 'assignments',
+      headerName: 'Asignaciones',
+      type: 'number',
+      width: 160,
+      renderHeader: () => <div className="header-bold">Asignaciones</div>,
     },
   ];
 
@@ -68,6 +233,55 @@ export default function GeneralTable() {
       channel: 'FACEBOOK',
       company,
       totalSpend: `$${totalSpend.toLocaleString('en-US')}`,
+      cpc: `$${
+        company === 'HOGARES UNION'
+          ? (
+              huAccounts.reduce(
+                (acc, curr) => acc + parseFloat(curr?.cpc ?? 0),
+                0
+              ) / huAccounts.length
+            ).toFixed(2)
+          : company === 'HU LIFESTYLE'
+          ? (
+              huLifestyleAccounts.reduce(
+                (acc, curr) => acc + parseFloat(curr?.cpc ?? 0),
+                0
+              ) / huLifestyleAccounts.length
+            ).toFixed(2)
+          : company === 'GIM LIVINGSPACES'
+          ? (
+              gimAccounts.reduce(
+                (acc, curr) => acc + parseFloat(curr?.cpc ?? 0),
+                0
+              ) / gimAccounts.length
+            ).toFixed(2)
+          : 0
+      }`,
+      leads:
+        company === 'HOGARES UNION'
+          ? huLeads
+          : company === 'HU LIFESTYLE'
+          ? huLifestyleLeads
+          : company === 'GIM LIVINGSPACES'
+          ? gimLeads
+          : 0,
+      cpl: `$${
+        company === 'HOGARES UNION'
+          ? (totalSpend / huLeads).toFixed(2)
+          : company === 'HU LIFESTYLE'
+          ? (totalSpend / huLifestyleLeads).toFixed(2)
+          : company === 'GIM LIVINGSPACES'
+          ? (totalSpend / gimLeads).toFixed(2)
+          : 0
+      }`,
+      assignments:
+        company === 'HOGARES UNION'
+          ? huAssignments
+          : company === 'HU LIFESTYLE'
+          ? huLifestyleAssignments
+          : company === 'GIM LIVINGSPACES'
+          ? gimAssignments
+          : 0,
     })
   );
 
@@ -81,7 +295,50 @@ export default function GeneralTable() {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     })}`,
+    cpc: `$${(
+      GoogleCPCByAccount(since, until, company.id) / 1000000
+    ).toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}`,
+    leads: GoogleLeadsByAccount(since, until, company.id).toFixed(0),
+    cpl: `$${(
+      GoogleSpendByAccount(since, until, company.id) /
+      1000000 /
+      GoogleLeadsByAccount(since, until, company.id)
+    ).toFixed(2)}`,
+    assignments: GoogleDataByAccount(since, until, company.id)
+      .map((company) => {
+        const result = contactsbyGoogleCampaign.filter(
+          (campaign) =>
+            campaign.hs_analytics_first_url === company.campaign.id.toString()
+        );
+        return result;
+      })
+      .flat().length,
   }));
+
+  const totalGoogleLeads = parseInt(
+    googleAccounts
+      .map((company) => GoogleLeadsByAccount(since, until, company.id))
+      .reduce((acc, curr) => acc + curr, 0)
+  );
+
+  const totalGoogleAssignments = googleAccounts
+    .map((company) => {
+      const googleContacts = GoogleDataByAccount(since, until, company.id).map(
+        (company) => {
+          const result = contactsbyGoogleCampaign.filter(
+            (campaign) =>
+              campaign.hs_analytics_first_url === company.campaign.id.toString()
+          );
+          return result;
+        }
+      );
+      return googleContacts;
+    })
+    .flat()
+    .reduce((acc, arr) => acc + arr.length, 0);
 
   const totalSpendSum = googleAccounts.reduce((sum, company) => {
     const totalSpend = (
@@ -94,10 +351,20 @@ export default function GeneralTable() {
     return sum + parseFloat(totalSpend.replace(',', ''));
   }, 0);
 
-  const fbFooterRow = {
+  const FooterRow = {
     id: 'grand-total',
     channel: 'GRAND TOTAL',
     totalSpend: `$${(grandTotalSpend + totalSpendSum).toLocaleString('en-US')}`,
+    leads: huLeads + huLifestyleLeads + gimLeads + totalGoogleLeads,
+    cpl: `$${(
+      (grandTotalSpend + totalSpendSum) /
+      (huLeads + huLifestyleLeads + gimLeads + totalGoogleLeads)
+    ).toFixed(2)}`,
+    assignments:
+      huAssignments +
+      huLifestyleAssignments +
+      gimAssignments +
+      totalGoogleAssignments,
   };
 
   return (
@@ -114,7 +381,7 @@ export default function GeneralTable() {
 
         <div className="table-container">
           <DataGrid
-            rows={[...fbRows, ...googleRows, fbFooterRow]}
+            rows={[...fbRows, ...googleRows, FooterRow]}
             columns={fbColumns}
             checkboxSelection
             components={{ Toolbar: GridToolbar }}
